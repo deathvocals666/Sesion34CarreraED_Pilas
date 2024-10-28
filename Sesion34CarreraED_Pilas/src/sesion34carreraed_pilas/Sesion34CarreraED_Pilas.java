6a7801c7466adbe9adbbbb21ec7a91204bc0a1ed
Victor Brito <deathvocals666@gmail.com.mx>
2024-10-28 15:36:28 -0600

    confirmacion 1 Carrera Brito
    Datos de desarrollo : Carrera Brito

---
+++ Sesion34CarreraED_Pilas/src/sesion34carreraed_pilas/Sesion34CarreraED_Pilas.java
@@ -0,0 +1,26 @@
+
+package sesion34carreraed_pilas;
+
+import java.util.Stack;
+import javax.swing.JOptionPane;
+
+
+public class Sesion34CarreraED_Pilas {
+
+    
+    public static void main(String[] args) {
+        //declaracion de pila 
+        Stack<Integer> pilaCarrera = new Stack <>();
+        
+        pilaCarrera.push(100);
+        pilaCarrera.push(150);
+        pilaCarrera.push(230);
+        pilaCarrera.push(10);
+        pilaCarrera.push(160);
+        
+        JOptionPane.showMessageDialog(null  , "Datos de la pila ordenada es: \n"+pilaCarrera);
+        
+        
+    }
+    
+}
